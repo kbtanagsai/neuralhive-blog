5192253623c242953a7c1cc6c3aef97f621e4d8b
K B Tanag Sai <aakulatanagsai23@gmail.com>
2025-11-30 23:44:20 +0530

    fix: correct JSX map syntax in index.js

--- pages/index.js
+++ pages/index.js
@@ -5,22 +5,22 @@ import { getAllPosts } from '../lib/posts'
 export default function Home({ posts }) {
   return (
     <Layout>
-      <h1 className="text-3xl font-mono mb-2"># ML MONDAYS</h1>
+      <h1 className="text-3xl font-mono mb-2"># HI, MONDAY</h1>
       <p className="mb-6">[Insert short About section]</p>
 
-      {posts.map(p => (
+      {posts.map((p) => (
         <PostCard
           key={p.slug}
           meta={p.meta}
-          excerpt={p.meta.excerpt || p.content.slice(0,150) + '...'}
+          excerpt={p.meta.excerpt || p.content.slice(0, 150) + '...'}
           href={/posts/${p.slug}}
         />
       ))}
     </Layout>
-  )
+  );
 }
 
-export async function getStaticProps(){
-  const posts = getAllPosts()
-  return { props: { posts } }
+export async function getStaticProps() {
+  const posts = getAllPosts();
+  return { props: { posts } };
 }
